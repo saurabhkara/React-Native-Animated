7f2f2e3843752d5ee3a63b9302a7542ce8e46540
wovvmac088 <wovvmac088@wovvmac088s-MacBook-Pro.local>
2023-06-08 11:54:44 +0530

    gesture handler

--- src/screens/ScrollAnimation.tsx
+++ src/screens/ScrollAnimation.tsx
@@ -3,15 +3,47 @@ import React from 'react';
 import {
   GestureHandlerRootView,
   PanGestureHandler,
+  PanGestureHandlerGestureEvent,
 } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 export default function ScrollAnimation() {
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
+  const gestureHandler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {startX: number; startY: number}
+  >({
+    onStart: (e, c) => {
+      c.startX = x.value;
+      c.startY = y.value;
+    },
+    onActive: (event, ctx) => {
+      x.value = ctx.startX + event.translationX;
+      y.value = ctx.startY + event.translationY;
+    },
+    onEnd: () => {
+      x.value = withSpring(0);
+      y.value = withSpring(0);
+    },
+  });
+
+  const animatedStylev = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: x.value}, {translateY: y.value}],
+    };
+  });
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style={{flex: 1}}>
       <View style={styles.container}>
         <Text style={{textAlign: 'center'}}>ScrollAnimation</Text>
-        <PanGestureHandler>
-          <View style={styles.square} />
+        <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[styles.square, animatedStylev]} />
         </PanGestureHandler>
       </View>
     </GestureHandlerRootView>
